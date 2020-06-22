e474d91287c614ba8b8af13d0c794cdb4d7c7a48
dams <damien.messner@gmail.com>
2020-06-22 23:39:55 +0200

    la suite

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,17 +1,48 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Trouve le nombre:");
 
-    let secret = rand::thread_rng().gen_range(1, 101);
+    let mut secret = rand::thread_rng().gen_range(1, 101);
+    for _elem in 1..100 {
+        secret = rand::thread_rng().gen_range(1, 101);
+        println!("Entre un nombre: {}", secret);
+    }
+    loop {
+        println!("Entre un nombre: {}", secret);
 
-    println!("Entre un nombre: {}", secret);
+        let mut chaine = String::new();
+        let retour = io::stdin().read_line(&mut chaine);
 
-    let mut chaine = String::new();
-    let retour = io::stdin().read_line(&mut chaine);
-    //.expect("ça a planté");
-    assert!(retour.is_ok());
+        assert!(retour.is_ok());
 
-    println!("tu as entré {}, retour expect {}", chaine, retour.expect("oush"));
+        println!(
+            "tu as entré {}, retour expect {}",
+            chaine,
+            retour.expect("oush")
+        );
+        let valeur = match chaine.trim().parse() {
+            /*Ok(50) => {
+                println!("tu as entrée 50?");
+                50;
+            }*/
+            Ok(num) => num,
+            
+            Err(_) => {
+                println!("tu t'es planté connard!!!");
+                continue;
+            }
+        };
+
+        match secret.cmp(&valeur) {
+            Ordering::Greater => println!("plus grand"),
+            Ordering::Less => println!("plus petit"),
+            Ordering::Equal => {
+                println!("égal");
+                break;
+            }
+        }
+    }
 }
